5ccd22b256a7b33b765a997398ef26a83d9fab08
Nikita Misko <mikita.misko@veraxen.com>
2024-09-05 21:04:12 +0300

    Add new start ball logic

--- Assets/Scripts/Game/Ball.cs
+++ Assets/Scripts/Game/Ball.cs
@@ -8,6 +8,10 @@ namespace Arkanoid.Game
 
         [SerializeField] private Rigidbody2D _rb;
         [SerializeField] private Vector2 _startDirection;
+        [SerializeField] private float _speed = 10;
+
+        private bool _isStarted;
+        private Platform _platform;
 
         #endregion
 
@@ -15,7 +19,55 @@ namespace Arkanoid.Game
 
         private void Start()
         {
-            _rb.velocity = _startDirection;
+            _platform = FindObjectOfType<Platform>();
+        }
+
+        private void Update()
+        {
+            if (_isStarted)
+            {
+                Debug.Log($"Velocity Magnitude: '{_rb.velocity.magnitude}'");
+                return;
+            }
+
+            MoveWithPlatform();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartFlying();
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!_isStarted)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, transform.position + (Vector3)_startDirection);
+                // Debug.Log($"Magnitude: '{_startDirection.magnitude}'");
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, transform.position + (Vector3)_rb.velocity);
+            }
+        }
+
+        #endregion
+
+        #region Private methods
+        
+        private void MoveWithPlatform()
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.x = _platform.transform.position.x;
+            transform.position = currentPosition;
+        }
+
+        private void StartFlying()
+        {
+            _isStarted = true;
+            _rb.velocity = _startDirection.normalized * _speed;
         }
 
         #endregion
